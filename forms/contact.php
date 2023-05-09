ad497ccedca2c43ef0e1da9f41428477d2b659c6
Da7238 <da7238@rit.edu>
2023-05-09 16:19:16 -0400

    Added google contact sheet and project images.

--- forms/contact.php
+++ forms/contact.php
@@ -24,14 +24,14 @@
   $contact->subject = $_POST['subject'];
 
   // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
-  /*
+
   $contact->smtp = array(
-    'host' => 'example.com',
-    'username' => 'example',
-    'password' => 'pass',
-    'port' => '587'
+    'host' => 'dardila137@gmail.com',
+    'username' => 'dardila137@gmail.com',
+    'password' => 'xxdshkrpyjjnomwm',
+    'port' => '456'
   );
-  */
+
 
   $contact->add_message( $_POST['name'], 'From');
   $contact->add_message( $_POST['email'], 'Email');
